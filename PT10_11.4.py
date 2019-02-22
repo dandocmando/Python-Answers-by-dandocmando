f4686209045e973af3f1e15ee8754a7f673f4761
dandocmando <danferguson2002@gmail.com>
2019-02-22 21:05:57 +1100

    first commit 11.4

--- PT10_11.4.py
+++ PT10_11.4.py
@@ -6,7 +6,7 @@ def l():
     time.sleep(1)
     for count in reversed(range(1, count+1)):
         time.sleep(1)
-        print("You have {0} seconds left".format(count))
+        print("You have {0} seconds left.".format(count))
     time.sleep(1)
     print()
     print("CountDown has finished!")
