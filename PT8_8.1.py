1c00821c11ce2630dca91e785b0b2da82a3a401d
dandocmando <danferguson2002@gmail.com>
2019-02-22 17:29:20 +1100

    test

---
+++ PT8_8.1.py
@@ -0,0 +1,7 @@
+def main():
+    print("What would you like to loop?")
+    dummy = input()
+    print("How many times would you like too loop it?")
+    i = int(input())
+    for counter in range(i):
+        print(dummy)
