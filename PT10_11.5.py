5f79ca68b45343a078c5d3f501ec3a6872c1aabf
dandocmando <danferguson2002@gmail.com>
2019-02-22 20:42:40 +1100

    first commit 11.5

--- PT10_11.5.py
+++ PT10_11.5.py
@@ -4,11 +4,11 @@ def l():
     print("This program checks that a given character is in a given string")
     print()
     time.sleep(1)
-    print("Input a String")
+    print("Input a String:")
     string = input()
     print()
     time.sleep(1)
-    print("Input a Character")
+    print("Input a Character:")
     character = input()
     time.sleep(1)
     if character in string:
