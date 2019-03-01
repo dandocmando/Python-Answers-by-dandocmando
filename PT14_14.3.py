19bf6f2c805788070601a0f1fc924a765e3b0657
dandocmando <danferguson2002@gmail.com>
2019-03-01 22:41:31 +1100

    first commit 14.3

--- PT14_14.3.py
+++ PT14_14.3.py
@@ -4,7 +4,7 @@ def l():
     print("This program will ask you to input a list of numbers and find the average of those numbers.")
     print()
     time.sleep(1)
-    print("Please input your list of numbers with a space in-between each")
+    print("Please input your list of numbers with a space in-between each:")
     arr = input()
     lst = list(map(int,arr.split(' ')))
     avg = sum(lst) / float(len(lst))
